29aa45c3f3c068f31fb0c3f1d5cb16ef1646ae4a
muttaki1251998 <muttaki4989@gmail.com>
2018-10-10 15:56:52 -0400

    Second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import {FormGroup, FormControl, InputGroup, Glyphicon} from 'react-bootstrap';
+import {FormGroup, FormControl, InputGroup} from 'react-bootstrap';
 
 class App extends Component{
 
@@ -13,7 +13,27 @@ constructor(props){
 
 search(){
   console.log('this.state', this.state);
-}
+  const BASE_URL = 'https://api.spotify.com/v1/search?';
+  const FETCH_URL = BASE_URL + 'q=' + this.state.query + '&type=artist&limit=1'
+                     console.log('FETCH_URL', FETCH_URL);
+
+  var accessToken = 'BQCzh4vxLxQTf6_z9dh-SA-NsOsPtSU7SU7unc_A_Xk62a-EUWqyFKOVLI_uuGM9ZwlerPusgmEssaoMT2JPMG_GFkSa9suzQnr_Ux4eMHYpmhN7x61j7yOxNeun5Qz5lBYBvBg6FuenOfGb11y5YhJXRMuZ4BBQjrRlDGnHU32Sku7NipxO&refresh_token=AQDT__GvXCshj7t8Ob6CIvapSugqsh2o1MBQ5QxEr_A04-0wSKPrvhSHiRJXGWnKO9sRGIW_avJ5PwneILLe1S92D8Qf7_mITkrDSRJcCiOu7QNEUy75ZOthb7AMyLyTamiUTQ';
+  //var myHeaders = new Headers();
+
+  var myOptions = {
+     method: 'GET',
+     headers: {
+         'Authorization':'Bearer ' + accessToken
+     },
+     mode: 'cors',
+     cache: 'default'
+   };
+
+   fetch(FETCH_URL, myOptions)
+     .then(response => response.json())
+     .then(json => console.log(json))
+
+ }
 
   render(){
   return(
@@ -26,9 +46,9 @@ search(){
           <FormControl
             type = "text"
             placeholder = "Search for an artist"
-             value = {this.state.query}
-             onChange = {event => this.setState({query: event.target.value})}
-             onKeyPress = {event => {
+            value = {this.state.query}
+            onChange = {event => this.setState({query: event.target.value})}
+            onKeyPress = {event => {
                if(event.key === 'Enter'){
                  this.search();
                }
